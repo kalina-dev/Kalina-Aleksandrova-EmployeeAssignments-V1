7926ca58bf4e662da0c2443721877704cae0f8d9
kalina-dev <110587154+kalina-dev@users.noreply.github.com>
2025-04-13 12:21:14 +0300

    clean redundant files

--- EmployeeAssignments.UI/src/app/layout/footer/footer.component.ts
+++
@@ -1,9 +0,0 @@
-// src/app/layout/footer/footer.component.ts
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
-})
-export class FooterComponent {}
--- EmployeeAssignments.UI/src/app/layout/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-// src/app/layout/header/header.component.ts
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
-})
-export class HeaderComponent {}
